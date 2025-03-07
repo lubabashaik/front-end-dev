89268097c8ff6bea27ac59d035cc3b009917bfd3
lubabashaik <lubishaik@gmail.com>
2025-03-07 16:42:37 +0530

    card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// Unable to resolve path to module '/vite.svg'.eslintimport/no-unresolved
-const viteLogo = '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-  // 'test' is assigned a value but never used.eslint@typescript-eslint/no-unused-vars
-  // const test = 123
-  // console.log('aaa')
-
-  // const f = [1, 2, 3, 4, 5].filter((n) => n % 2 === 0)
-
-  return (
-    <>
-      <div>
-        {/* Using target="_blank" without rel="noreferrer" (which implies rel="noopener") is a security risk in older browsers: see https://mathiasbynens.github.io/rel-noopener/#recommendationseslintreact/jsx-no-target-blank */}
-        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      {/* img elements must have an alt prop, either with meaningful text, or an empty string for decorative images.eslintjsx-a11y/alt-text */}
-      <img src="" alt="" />
-      <h1>
-        <button type="button">Click Me</button>
-      </h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  )
+  return <Card />
 }
 
 export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+function Card() {
+  return (
+    <div className="card-box">
+      <div className="header">
+        <div className="Balance-details">
+          <p className="heading">Balance</p>
+          <h4 className="content">$5,756</h4>
+        </div>
+        <img className="image-icon" src="chip-icon.png" alt="icon" />
+      </div>
+      <div className="user-details">
+        <div className="user-name">
+          <p className="heading">CARD HOLDER</p>
+          <h4 className="content">Eddy Cusuma</h4>
+        </div>
+        <div className="expiry-date">
+          <p className="heading">VALID THRU</p>
+          <h4 className="content">12/22</h4>
+        </div>
+      </div>
+      <div className="footer">
+        <h3 className="Number">3778**** ****1234</h3>
+        <span className="footer-icon">
+          <div className="crc-1"></div>
+          <div className="crc-2"></div>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Card
